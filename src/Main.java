0a298423f47922be85a7390ea80e062f50d3494b
Max <eparis@atlassian.com>
2022-12-29 13:47:35 +0400

    initial commit2

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int amount = 2120;
+        int persent = 20;
+        int bonus = amount / persent;
+
+        System.out.println("Количество начисленных миль - " + bonus);
+
+    }
+}
